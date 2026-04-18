ce12347306f026d4aa37936b09fa53b61071c4dd
zpect-dev <jvbertaggia@gmail.com>
2026-04-18 18:47:31 -0300

    aplicacion contenerizada y api creada para manejar los datos de manera externa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             EmpresaSeeder::class,
             InversionSeeder::class,
             VehiculoSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
