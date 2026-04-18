a2d6e2c4a685dccd0ca29e0882cc17f1ced483fc
zpect-dev <jvbertaggia@gmail.com>
2026-04-18 18:53:23 -0300

    aplicacion contenerizada y api creada para manejar los datos de manera externa

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
